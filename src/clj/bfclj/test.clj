1881d550f2943f1a33a80444f9f808f66d771c58
deciduously <github@deciduously.com>
2018-03-13 19:37:45 -0400

    Better tests

--- src/clj/bfclj/test.clj
+++ src/clj/bfclj/test.clj
@@ -1,20 +1,52 @@
 (ns bfclj.test
   (:require [bfclj.op :as op]
+            [bfclj.read :as r]
             [bfclj.machine :refer [fresh]]
             [bfclj.test :as sut]
-            [clojure.test :refer [deftest is run-tests]]))
+            [clojure.test :refer [deftest is run-tests testing]]))
 
 (deftest test-inc-cell
-  (is (= {:tape [1] :current 0} (op/inc-cell fresh)))
-  (is (= {:tape [1 1] :current 0} (op/inc-cell {:tape [0 1] :current 0}))))
+  (testing "inc-cell"
+    (testing "Fresh machine"
+      (is (= {:tape [1] :current 0} (op/inc-cell fresh))))
+    (testing "Bigger tape"
+      (is (= {:tape [1 1] :current 0} (op/inc-cell {:tape [0 1] :current 0}))))))
 
 (deftest test-dec-cell
-  (is (= {:tape [-1] :current 0} (op/dec-cell fresh)))
-  (is (= {:tape [0 0] :current 1} (op/dec-cell {:tape [0 1] :current 1}))))
+  (testing "dec-cell"
+    (testing "Fresh machine"
+      (is (= {:tape [-1] :current 0} (op/dec-cell fresh))))
+    (testing "Bigger tape"
+      (is (= {:tape [0 0] :current 1} (op/dec-cell {:tape [0 1] :current 1}))))))
 
 (deftest test-inc-current
-  (is (= {:tape [0 0] :current 1} (op/inc-current fresh)))
-  (is (= {:tape [0 1] :current 1} (op/inc-current {:tape [0 1] :current 0}))))
+  (testing "inc-current"
+    (testing "Fresh machine"
+      (is (= {:tape [0 0] :current 1} (op/inc-current fresh))))
+    (testing "Bigger tape"
+      (is (= {:tape [0 1] :current 1} (op/inc-current {:tape [0 1] :current 0}))))))
 
 (deftest test-dec-current
   (is (= {:tape [0 0] :current 0} (op/dec-current {:tape [0 0] :current 1}))))
+
+(deftest test-read
+  (testing "Read"
+    (testing "Ends with a newline?"
+      (is (r/ends-with-newline? "....\n"))
+      (is (not (r/ends-with-newline? "..."))))
+    (testing "All valid tokens?"
+      (testing "One of each"
+        (is (r/valid-tokens "+-[],.<>\n")))
+      (testing "One invalid token?"
+        (is (not (r/valid-tokens "+-[],;.<>\n")))))
+    (testing "Valid loops?"
+      (testing "Correct loop"
+        (is (r/loops-valid? "...[...]...\n")))
+      (testing "Unmatched bracket"
+        (is (not (r/loops-valid? "...[...\n"))))
+      (testing "One correct, one unmatched"
+        (is (not (r/loops-valid? "...[..].[..\n"))))
+      (testing "Two correct"
+        (is (r/loops-valid? "..[.]..[...].\n")))
+      (testing "No loops"
+        (is (r/loops-valid? "...\n"))))))
